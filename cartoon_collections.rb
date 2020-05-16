ff2af680875da344d187a3b6ff0b6fdb3dcbb8ad
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-16 10:34:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  
+  puts array.each_with_index.collect { |name, index| "#{index + 1} #{name}" } 
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  return array.collect {|phrase| phrase.capitalize << "!"}
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  array.include?()
+  
 end
 
 def find_the_cheese# code an argument here
