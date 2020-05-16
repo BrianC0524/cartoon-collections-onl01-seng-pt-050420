e15974d0edeb6822f8685ff0c7d273a96d89e959
Brian Craycraft <craycraftb24@hotmail.com>
2020-05-16 11:08:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   
   
   
-  true if array.include? array.length > 4
+  true if array.any? array.length > 4
     
   false if array.all? {|word| word.length <= 4}
   
